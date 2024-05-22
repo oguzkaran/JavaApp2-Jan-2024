8bb9f9ddd0f54b885d1431d637109ca965c6eaa8
Oğuz Karan <oguzkaran@csystem.org>
2024-05-22 17:28:12 +0300

    Update PaymentServerFactory.java

--- src/Projects/PaymentSystem/PaymentManagerServer/src/main/java/org/csystem/app/payment/server/manager/client/factory/PaymentServerFactory.java
+++ src/Projects/PaymentSystem/PaymentManagerServer/src/main/java/org/csystem/app/payment/server/manager/client/factory/PaymentServerFactory.java
@@ -8,29 +8,41 @@ import java.util.Optional;
 
 public class PaymentServerFactory {
     //...
-    private Optional<PaymentServerInfo> socketServerCallback(int id, String host, String [] info)
+    private Optional<PaymentServerInfo> socketServerCallback(int id, String host, String[] info)
     {
-        //...
-        return Optional.of(new SocketPaymentServerInfo(id, host, Integer.parseInt(info[1])));
+        try {
+            return Optional.of(new SocketPaymentServerInfo(id, host, Integer.parseInt(info[1])));
+        } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+            return Optional.empty();
+        }
     }
 
     private Optional<PaymentServerInfo> restServiceCallback(int id, String [] info)
     {
-        return Optional.of(new RestPaymentServiceInfo(id, info[1]));
+        try {
+            return Optional.of(new RestPaymentServiceInfo(id, info[1]));
+        } catch (IndexOutOfBoundsException ex) {
+            return Optional.empty();
+        }
     }
 
-    public Optional<PaymentServerInfo> create(int id, String host, String infoStr)
-    {
-        //S;2323
-        //R:http://......
-        //G:
-        var info = infoStr.split("[;]");
 
-        //info'ların ilgili bilgileri kontrol edilecektir
+    private Optional <PaymentServerInfo> protocolCallback(int id, String host, String [] info)
+    {
         return switch (info[0]) {
             case "S" -> socketServerCallback(id, host, info);
             case "R" -> restServiceCallback(id, info);
             default -> Optional.empty();
         };
+    }
+    public Optional<PaymentServerInfo> create(int id, String host, String infoStr)
+    {
+        //S;2323
+        //R:http://......
+        //G:
+        //info'ların ilgili bilgileri kontrol edilecektir
+        var info = infoStr.split("[;]");
+
+        return info.length >= 2 ? protocolCallback(id, host, info) : Optional.empty();
     }
 }
