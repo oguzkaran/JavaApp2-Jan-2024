9c2bd3314159f75c733abd83a96a4d939756d189
Oğuz Karan <oguzkaran@csystem.org>
2024-12-02 21:54:23 +0300

    upddate

--- src/Projects/041-DemoMessageStorageMongoDB/MongoRepositoryLib/src/main/java/org/csystem/app/message/repository/IMessageRepository.java
+++ src/Projects/041-DemoMessageStorageMongoDB/MongoRepositoryLib/src/main/java/org/csystem/app/message/repository/IMessageRepository.java
@@ -1,7 +1,6 @@
 package org.csystem.app.message.repository;
 
 import org.csystem.app.message.repository.document.Message;
-import org.springframework.data.domain.Example;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,8 +9,9 @@ import java.util.List;
 
 @Repository
 public interface IMessageRepository extends MongoRepository<Message, String> {
+    @Query("{'from': ?0, 'to':  ?1}")  //That is not needed. Just used for demonstration
     List<Message> findByFromAndTo(String from, String to);
 
-    @Query("{}")
-    List<Message> findByContentAsTextContains(String text);
+    @Query("{ '$text': {'$search':  \"?0\"} }") //That is not needed. Just used for demonstration
+    List<Message> findByContentContains(String text);
 }
--- src/Projects/041-DemoMessageStorageMongoDB/MongoRepositoryLib/src/test/java/org/csystem/app/message/repository/MessageRepositoryTest.java
+++ src/Projects/041-DemoMessageStorageMongoDB/MongoRepositoryLib/src/test/java/org/csystem/app/message/repository/MessageRepositoryTest.java
@@ -1,13 +1,14 @@
 package org.csystem.app.message.repository;
 
-
 import org.csystem.app.message.repository.document.Message;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootApplication
 @SpringBootTest
@@ -16,17 +17,40 @@ public class MessageRepositoryTest {
     @Autowired
     private IMessageRepository m_messageRepository;
 
+    private void setupData()
+    {
+        m_messageRepository.insert(Message.builder().from("ali").to("veli").content("Hello Veli").build());
+        m_messageRepository.insert(Message.builder().from("ali").to("veli").content("Fine thanks").build());
+        m_messageRepository.insert(Message.builder().from("ali").to("veli").content("Where are you?").build());
+        m_messageRepository.insert(Message.builder().from("veli").to("ali").content("Hi. How are you?").build());
+        m_messageRepository.insert(Message.builder().from("veli").to("ali").content("thanks?").build());
+    }
+
     @BeforeEach
     public void setup()
     {
         m_messageRepository.deleteAll();
-        m_messageRepository.insert(Message.builder().from("ali").to("veli").content("Hello Veli").build());
-        m_messageRepository.insert(Message.builder().from("veli").to("ali").content("Hi. How are you?").build());
+        setupData();
     }
 
     @Test
-    public void test()
+    public void givenValues_whenFromAndTo_thenFindMessageSize()
     {
-        //...
+        var expectedSize = 3;
+        var from = "ali";
+        var to = "veli";
+        var messages = m_messageRepository.findByFromAndTo(from, to);
+
+        assertEquals(expectedSize, messages.size());
+    }
+
+    @Test
+    public void givenValues_whenTextContains_thenFindMessageSize()
+    {
+        var expectedSize = 2;
+        var text = "thanks";
+        var messages = m_messageRepository.findByContentContains(text);
+
+        assertEquals(expectedSize, messages.size());
     }
 }
